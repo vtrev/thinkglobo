2c4a6d7575b176543a34120bcc9063c35b2cd27d
vtrev <baloyi.trev@gmail.com>
2019-06-18 17:44:37 +0200

    added almost all content

--- src/javascript.js
+++ src/javascript.js
@@ -2,13 +2,30 @@ window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
 
 
-    $(window).scroll(function(){
+    $(window).scroll(function () {
         $('nav').toggleClass('scrolled', $(this).scrollTop() > 20);
     });
 
     $('.carousel').carousel({
-        interval:2500,
+        interval: 2500,
         pause: false
 
-      })
+    });
+
+});
+
+$(document).ready(function () {
+    $('.client-logos').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        // fade: true,
+        infinite: true,
+        // dots: true,
+
+
+
+        // 'setting-name': setting - value
+    });
 });
