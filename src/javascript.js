32487fe3eb40506ec77e4078680c30adedeff96d
vtrev <baloyi.trev@gmail.com>
2019-06-19 17:40:00 +0200

    added client logos

--- src/javascript.js
+++ src/javascript.js
@@ -25,31 +25,31 @@ $(document).ready(function () {
 
     $('.client-logos').slick({
         slidesToShow: 3,
-        slidesToScroll: 2,
+        slidesToScroll: 3,
         autoplay: true,
         autoplaySpeed: 2000,
         infinite: true,
         responsive: [{
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
                     infinite: true,
-                    dots: true
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
+                    dots: false,
                 }
             }
             // You can unslick at a given breakpoint now by adding:
